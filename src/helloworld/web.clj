497b91c523c69db3dbb1d7b76ff515e81f73357d
Ben Grunfeld <benjamin.grunfeld@gmail.com>
2013-10-28 15:00:53 -0600

    Added support for an environment variable

--- src/helloworld/web.clj
+++ src/helloworld/web.clj
@@ -1,19 +1,19 @@
-(ns helloworld.web 
-    (:use compojure.core [ring.adapter.jetty :only [run-jetty]] )
-    (:require [compojure.route :as route]
-              [compojure.handler :as handler]))
+	(ns helloworld.web 
+	    (:use compojure.core [ring.adapter.jetty :only [run-jetty]] )
+	    (:require [compojure.route :as route]
+	              [compojure.handler :as handler]))
+
+	(def message (System/getenv "POWERED_BY"))	
+
+	(defroutes main-routes
+	  ; what's going on
+	  	(GET "/" [] (apply str "Powered by " message))
+	    (route/resources "/")
+	    (route/not-found "Page not found")   )
 
 
-(defroutes main-routes
-  ; what's going on
-
-    (GET "/" [] (apply str "Powered by " (System/getenv "POWERED_BY") ) )
-    (route/resources "/")
-    (route/not-found "Page not found")   )
-
-
-(def app
-    (handler/api main-routes))
-       
-(defn -main [port]
-    (run-jetty app {:port (Integer. port)}))
+	(def app
+	    (handler/api main-routes))
+	       
+	(defn -main [port]
+	    (run-jetty app {:port (Integer. port)}))
